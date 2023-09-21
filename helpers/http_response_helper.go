1ad1bfa5c088e29c653e933c35c9ce2c95388669
washington-shoji <shoji.washington@gmail.com>
2023-09-21 20:51:05 +1000

    -added web response handler helper

--- helpers/http_response_helper.go
+++ helpers/http_response_helper.go
@@ -1,8 +1,20 @@
 package helpers
 
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
 type HttpResponse struct {
 	Status interface{} `json:"status"`
 	Data   interface{} `json:"data,omitempty"`
+}
+
+type Response struct {
+	Status  int
+	Message []string
+	Error   []string
 }
 
 func WebResponse(status interface{}, data interface{}) *HttpResponse {
@@ -11,4 +23,12 @@ func WebResponse(status interface{}, data interface{}) *HttpResponse {
 		Data:   data,
 	}
 	return res
+}
+
+func WebResponseHandler(c *gin.Context, response Response) {
+	if len(response.Message) > 0 {
+		c.JSON(response.Status, map[string]interface{}{"message": strings.Join(response.Message, "; ")})
+	} else if len(response.Error) > 0 {
+		c.JSON(response.Status, map[string]interface{}{"error": strings.Join(response.Error, "; ")})
+	}
 }
