941f016b3bf34db2f3bef80d739079418d8da101
washington-shoji <shoji.washington@gmail.com>
2023-09-22 17:21:38 +1000

    -updated service with a book response type

--- services/book_service.go
+++ services/book_service.go
@@ -9,6 +9,6 @@ type BookService interface {
 	Create(book *models.CreateBookRequest) (error error)
 	Update(id uuid.UUID, book *models.UpdateBookRequest) (error error)
 	Delete(id uuid.UUID) (error error)
-	FindByID(id uuid.UUID, bkr *models.FindByIDBookRequest) (book *models.Book, error error)
-	FindAll() (books []*models.Book, error error)
+	FindByID(id uuid.UUID, bkr *models.FindByIDBookRequest) (book *models.BookResponse, error error)
+	FindAll() (books []*models.BookResponse, error error)
 }
