51e5157544888b0f73b2936a4529a2df3e4a039c
washington-shoji <shoji.washington@gmail.com>
2023-09-23 20:28:02 +1000

    -added user_account repository

---
+++ repositories/user_account_repository.go
@@ -0,0 +1,14 @@
+package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/washington-shoji/gin-api/models"
+)
+
+type UserAccountRepository interface {
+	Create(usrAcc *models.UserAccount) (error error)
+	Update(usrAcc *models.UserAccount) (error error)
+	Delete(usrAcc *models.UserAccount) (error error)
+	FindByID(id uuid.UUID) (usrAcc *models.UserAccount, error error)
+	FindAll() (usrAcc []*models.UserAccount, error error)
+}
