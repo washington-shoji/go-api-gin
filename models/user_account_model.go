a52ea9310466d0a0542b848eb4f994579008ca16
washington-shoji <shoji.washington@gmail.com>
2023-09-23 20:27:37 +1000

    -added user_account model

---
+++ models/user_account_model.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserAccount struct {
+	ID        uuid.UUID  `json:"id"`
+	Username  string     `json:"username"`
+	Password  string     `json:"password"`
+	Email     string     `json:"email"`
+	FullName  string     `json:"fullName"`
+	Role      string     `json:"role"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
+}
+
+type UserAccountResponse struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	FullName string    `json:"fullName"`
+}
+
+type CreateUserAccountRequest struct {
+	Username string `validate:"required" json:"username"`
+	Password string `validate:"required" json:"password"`
+	Email    string `validate:"required" json:"email"`
+	FullName string `validate:"required" json:"fullName"`
+}
+
+type UpdateUserAccountRequest struct {
+	Username string `validate:"required" json:"username"`
+	Password string `validate:"required" json:"password"`
+	Email    string `validate:"required" json:"email"`
+	FullName string `validate:"required" json:"fullName"`
+}
