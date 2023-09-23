c41a4a18cd86b72904afb0643ccab5170bed932c
washington-shoji <shoji.washington@gmail.com>
2023-09-24 09:59:45 +1000

    -added user account services interface

---
+++ services/user_account_service.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"github.com/google/uuid"
+	"github.com/washington-shoji/gin-api/models"
+)
+
+type UserAccountService interface {
+	Create(usrAcc *models.CreateUserAccountRequest) (error error)
+	Update(id uuid.UUID, usrAcc *models.UpdateBookRequest) (error error)
+	Delete(id uuid.UUID) (error error)
+	FindByID(id uuid.UUID) (usrAcc *models.UserAccountResponse, error error)
+	FindAll() (usrAcc []*models.UserAccountResponse, error error)
+}
