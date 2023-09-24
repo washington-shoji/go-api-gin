4099676bfa203161387185abcd5cad4a1cd3ccae
washington-shoji <shoji.washington@gmail.com>
2023-09-24 14:34:05 +1000

    -updated user account service interface

--- services/user_account_service.go
+++ services/user_account_service.go
@@ -7,7 +7,7 @@ import (
 
 type UserAccountService interface {
 	Create(usrAcc *models.CreateUserAccountRequest) (error error)
-	Update(id uuid.UUID, usrAcc *models.UpdateBookRequest) (error error)
+	Update(id uuid.UUID, usrAcc *models.UpdateUserAccountRequest) (error error)
 	Delete(id uuid.UUID) (error error)
 	FindByID(id uuid.UUID) (usrAcc *models.UserAccountResponse, error error)
 	FindAll() (usrAcc []*models.UserAccountResponse, error error)
