30e6ff1500942756b8e20dd6d417e52c8bb42ab2
washington-shoji <shoji.washington@gmail.com>
2023-09-26 19:19:48 +1000

    -updated router with login router and middleware

--- routers/router.go
+++ routers/router.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/washington-shoji/gin-api/handlers"
+	"github.com/washington-shoji/gin-api/middleware"
 )
 
 func NewRouter(
 	bookHandler *handlers.BookHandler,
 	userAccountHandler *handlers.UserAccountHandler,
+	loginHandler *handlers.LoginHandler,
 ) *gin.Engine {
 	service := gin.Default()
 
@@ -22,7 +24,12 @@ func NewRouter(
 	})
 
 	router := service.Group("/api")
+
+	loginRouter := router.Group("/login")
+	loginRouter.POST("", loginHandler.Login)
+
 	bookRouter := router.Group("/book")
+	bookRouter.Use(middleware.JwtAuthMiddleware())
 	bookRouter.GET("", bookHandler.GetAllBooks)
 	bookRouter.GET("/:id", bookHandler.GetBookByID)
 	bookRouter.POST("", bookHandler.Create)
@@ -31,7 +38,10 @@ func NewRouter(
 
 	userAccRouter := router.Group("/account")
 	userAccRouter.GET("", userAccountHandler.GetAllUserAccounts)
+	userAccRouter.GET("/:id", userAccountHandler.GetUserAccountByID)
 	userAccRouter.POST("", userAccountHandler.Create)
+	userAccRouter.PATCH("/:id", userAccountHandler.Update)
+	userAccRouter.DELETE("/:id", userAccountHandler.Delete)
 
 	return service
 }
