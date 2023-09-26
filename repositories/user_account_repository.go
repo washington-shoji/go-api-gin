4e6139324ed91f9095eea0281cf0750f3e8f4191
washington-shoji <shoji.washington@gmail.com>
2023-09-26 19:16:42 +1000

    -updated user account repo with find by username

--- repositories/user_account_repository.go
+++ repositories/user_account_repository.go
@@ -10,5 +10,6 @@ type UserAccountRepository interface {
 	Update(usrAcc *models.UserAccount) (error error)
 	Delete(usrAcc *models.UserAccount) (error error)
 	FindByID(id uuid.UUID) (usrAcc *models.UserAccount, error error)
+	FindByUsername(username string) (usrAcc *models.UserAccount, error error)
 	FindAll() (usrAcc []*models.UserAccount, error error)
 }
