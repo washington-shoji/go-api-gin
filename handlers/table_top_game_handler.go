dddca4232baac04d22092c1bc37d09963590fdd8
washington-shoji <shoji.washington@gmail.com>
2023-09-29 17:38:07 +1000

    -added table top game handler

---
+++ handlers/table_top_game_handler.go
@@ -0,0 +1,134 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/washington-shoji/gin-api/helpers"
+	"github.com/washington-shoji/gin-api/models"
+	"github.com/washington-shoji/gin-api/services"
+)
+
+type TableTopGameHandler struct {
+	Service services.TableTopGameService
+}
+
+func NewTableTopGameHandler(service services.TableTopGameService) *TableTopGameHandler {
+	return &TableTopGameHandler{
+		Service: service,
+	}
+}
+
+func (service *TableTopGameHandler) Create(ctx *gin.Context) {
+	createTblGameReq := models.TableTopGameReq{}
+	fmt.Println("createTblGameReq", createTblGameReq)
+	err := ctx.ShouldBindJSON(&createTblGameReq)
+	if err != nil {
+		fmt.Println("err 1", err)
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	if err := service.Service.Create(&createTblGameReq); err != nil {
+		fmt.Println("err 2", err)
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	resp := SuccessMessage{
+		Message: "Created successfully",
+	}
+
+	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: resp})
+}
+
+func (handler *TableTopGameHandler) Update(ctx *gin.Context) {
+	uptTblGameReq := models.TableTopGameReq{}
+	err := ctx.ShouldBindJSON(&uptTblGameReq)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	tblId := ctx.Param("id")
+	id, err := uuid.Parse(tblId)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	if err := handler.Service.Update(id, &uptTblGameReq); err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Table top game not found"}})
+		return
+	}
+
+	resp := SuccessMessage{}
+	resp.Message = "Updated successfully"
+
+	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: resp})
+}
+
+func (handler *TableTopGameHandler) Delete(ctx *gin.Context) {
+	uptTblGameReq := models.TableTopGameReq{}
+	err := ctx.ShouldBindJSON(&uptTblGameReq)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	tblId := ctx.Param("id")
+	id, err := uuid.Parse(tblId)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	if err := handler.Service.Delete(id); err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Table top game not found"}})
+		return
+	}
+
+	resp := SuccessMessage{}
+	resp.Message = "Deleted successfully"
+
+	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: resp})
+}
+
+func (handler *TableTopGameHandler) FindByID(ctx *gin.Context) {
+	findByIDReq := models.TableTopGameReq{}
+	err := ctx.ShouldBindJSON(&findByIDReq)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	tblId := ctx.Param("id")
+	id, err := uuid.Parse(tblId)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Invalid input"}})
+		return
+	}
+
+	result, err := handler.Service.FindByID(id)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadRequest, Error: []string{"Table top game not found"}})
+		return
+	}
+
+	ctx.Header("Content-Type", "application/json")
+	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: result})
+}
+
+func (handler *TableTopGameHandler) FindAll(ctx *gin.Context) {
+	result, err := handler.Service.FindAll()
+	if err != nil {
+		fmt.Println("err 3", err)
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusBadGateway, Error: []string{"Server Error"}})
+		return
+	}
+
+	ctx.Header("Content-Type", "application/json")
+	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: result})
+}
