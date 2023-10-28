d4b7e83a6d7a609d8de20053629164620ad200f9
washington-shoji <shoji.washington@gmail.com>
2023-10-29 10:52:26 +1100

    -updated login handler

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -39,3 +39,39 @@ func (handler *LoginHandler) Login(ctx *gin.Context) {
 	ctx.Header("Content-Type", "application/json")
 	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: result})
 }
+
+func (handler *LoginHandler) LoginRenderForm(ctx *gin.Context) {
+
+	ctx.HTML(http.StatusOK, "login", nil)
+}
+
+func (handler *LoginHandler) LoginRenderAuth(ctx *gin.Context) {
+
+	if err := ctx.Request.Form; err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{
+			Status: http.StatusBadRequest,
+			Error:  []string{"Invalid input"},
+		})
+		return
+	}
+
+	username := ctx.Request.FormValue("username")
+	password := ctx.Request.FormValue("password")
+
+	result, err := handler.LoginService.Login(username, password)
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusForbidden, Error: []string{"Not authorized"}})
+		return
+	}
+
+	ctx.SetCookie("jwt", result.Token, 3600, "/", "localhost", false, true)
+
+	ctx.Header("HX-Redirect", "/api/exp-html/base")
+}
+
+func (handler *LoginHandler) LogOutAuth(ctx *gin.Context) {
+
+	ctx.SetCookie("jwt", "", -3600, "/", "localhost", false, true)
+
+	ctx.Header("HX-Redirect", "/api/auth")
+}
