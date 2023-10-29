9ca42e4342428ebc946f4a87ef565df49238d8e4
washington-shoji <shoji.washington@gmail.com>
2023-10-29 20:08:51 +1100

    -added event repository

---
+++ repositories/event_repository.go
@@ -0,0 +1,14 @@
+package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/washington-shoji/gin-api/models"
+)
+
+type EventRepository interface {
+	Create(event *models.Event) error
+	Update(event *models.Event) error
+	Delete(id *uuid.UUID) error
+	FindByID(id *uuid.UUID) (*models.Event, error)
+	FindAll() ([]*models.Event, error)
+}
