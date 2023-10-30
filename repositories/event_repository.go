49312af6229bb1f9b6d49212cf393a472cd2c99f
washington-shoji <shoji.washington@gmail.com>
2023-10-31 06:47:52 +1100

    -added event repository

--- repositories/event_repository.go
+++ repositories/event_repository.go
@@ -6,9 +6,9 @@ import (
 )
 
 type EventRepository interface {
-	Create(event *models.Event) error
-	Update(event *models.Event) error
-	Delete(id *uuid.UUID) error
-	FindByID(id *uuid.UUID) (*models.Event, error)
+	Create(event models.Event) error
+	Update(event models.Event) error
+	Delete(id uuid.UUID) error
+	FindByID(id uuid.UUID) (*models.Event, error)
 	FindAll() ([]*models.Event, error)
 }
