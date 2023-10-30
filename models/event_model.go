b006b55032ec070fade2eef14eaa0fa2aaed2994
washington-shoji <shoji.washington@gmail.com>
2023-10-31 06:47:27 +1100

    -added event model

--- models/event_model.go
+++ models/event_model.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"mime/multipart"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,5 +17,25 @@ type Event struct {
 	Registration     time.Time  `json:"registration"`
 	CreatedAt        time.Time  `json:"createdAt"`
 	UpdatedAt        *time.Time `json:"updatedAt"`
-	DeletedAt        *time.Time `json:"deletedAt"`
+}
+
+type EventReq struct {
+	Title            string                `json:"title"`
+	ShortDescription string                `json:"short-description"`
+	Description      string                `json:"description"`
+	ImageFile        multipart.File        `json:"imageFile"`
+	ImageHeader      *multipart.FileHeader `json:"imageHeader"`
+	ImageUrl         string                `json:"image-url"`
+	Date             time.Time             `json:"data"`
+	Registration     time.Time             `json:"registration"`
+}
+
+type EventRes struct {
+	ID               uuid.UUID `json:"id"`
+	Title            string    `json:"title"`
+	ShortDescription string    `json:"short-description"`
+	Description      string    `json:"description"`
+	ImageUrl         string    `json:"image-url"`
+	Date             time.Time `json:"data"`
+	Registration     time.Time `json:"registration"`
 }
