949b99f852d07ff5217f4da05300b1b409931b2f
washington-shoji <shoji.washington@gmail.com>
2023-10-30 18:22:17 +1100

    -updated book model with image url param

--- models/book_model.go
+++ models/book_model.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"mime/multipart"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,6 +11,7 @@ type Book struct {
 	ID          uuid.UUID  `json:"id"`
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
+	ImageUrl    string     `json:"imageUrl"`
 	CreatedAt   time.Time  `json:"createdAt"`
 	UpdatedAt   *time.Time `json:"updatedAt"`
 	DeletedAt   *time.Time `json:"deletedAt"`
@@ -19,18 +21,25 @@ type BookResponse struct {
 	ID          uuid.UUID `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
+	ImageUrl    string    `json:"imageUrl"`
 }
 
 type CreateBookRequest struct {
-	Title       string `validate:"required" json:"title"`
-	Description string `validate:"required" json:"description"`
+	Title       string                `validate:"required" json:"title"`
+	Description string                `validate:"required" json:"description"`
+	ImageFile   multipart.File        `json:"imageFile"`
+	ImageHeader *multipart.FileHeader `json:"imageHeader"`
+	ImageUrl    string                `json:"imageUrl"`
 }
 
 type UpdateBookRequest struct {
-	ID          uuid.UUID  `validate:"required" json:"id"`
-	Title       string     `validate:"required,min=1,max=50" json:"title"`
-	Description string     `validate:"required,min=1,max=500" json:"description"`
-	UpdatedAt   *time.Time `json:"updatedAt"`
+	ID          uuid.UUID             `validate:"required" json:"id"`
+	Title       string                `validate:"required,min=1,max=50" json:"title"`
+	Description string                `validate:"required,min=1,max=500" json:"description"`
+	UpdatedAt   *time.Time            `json:"updatedAt"`
+	ImageFile   multipart.File        `json:"imageFile"`
+	ImageHeader *multipart.FileHeader `json:"imageHeader"`
+	ImageUrl    string                `json:"imageUrl"`
 }
 
 type DeleteBookRequest struct {
