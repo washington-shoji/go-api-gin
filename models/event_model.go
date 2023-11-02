4ed82da62c0d036ed54540d9a385f1cd646b56e6
washington-shoji <shoji.washington@gmail.com>
2023-11-03 07:21:07 +1100

    -updated the event model

--- models/event_model.go
+++ models/event_model.go
@@ -10,9 +10,9 @@ import (
 type Event struct {
 	ID               uuid.UUID  `json:"id"`
 	Title            string     `json:"title"`
-	ShortDescription string     `json:"short-description"`
+	ShortDescription string     `json:"shortDescription"`
 	Description      string     `json:"description"`
-	ImageUrl         string     `json:"image-url"`
+	ImageUrl         string     `json:"imageUrl"`
 	Date             time.Time  `json:"data"`
 	Registration     time.Time  `json:"registration"`
 	CreatedAt        time.Time  `json:"createdAt"`
@@ -20,22 +20,22 @@ type Event struct {
 }
 
 type EventReq struct {
-	Title            string                `json:"title" form:"title"`
-	ShortDescription string                `json:"short-description" form:"short-description"`
-	Description      string                `json:"description" form:"description"`
-	ImageFile        multipart.File        `json:"imageFile" form:"imageFile"`
-	ImageHeader      *multipart.FileHeader `json:"imageHeader" form:"imageHeader"`
-	ImageUrl         string                `json:"image-url" form:"image-url"`
-	Date             time.Time             `json:"data" form:"data"`
-	Registration     time.Time             `json:"registration" form:"registration"`
+	Title            string               `json:"title" form:"title"`
+	ShortDescription string               `json:"shortDescription" form:"shortDescription"`
+	Description      string               `json:"description" form:"description"`
+	ImageFile        multipart.File       `json:"imageFile" form:"imageFile"`
+	ImageHeader      multipart.FileHeader `json:"imageHeader" form:"imageHeader"`
+	ImageUrl         string               `json:"imageUrl" form:"imageUrl"`
+	Date             time.Time            `json:"date" form:"date"`
+	Registration     time.Time            `json:"registration" form:"registration"`
 }
 
 type EventRes struct {
 	ID               uuid.UUID `json:"id"`
 	Title            string    `json:"title"`
-	ShortDescription string    `json:"short-description"`
+	ShortDescription string    `json:"shortDescription"`
 	Description      string    `json:"description"`
-	ImageUrl         string    `json:"image-url"`
-	Date             time.Time `json:"data"`
+	ImageUrl         string    `json:"imageUrl"`
+	Date             time.Time `json:"date"`
 	Registration     time.Time `json:"registration"`
 }
