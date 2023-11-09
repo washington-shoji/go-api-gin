aa10bc7952f9174282b616d069ec91e29cf58e4d
washington-shoji <shoji.washington@gmail.com>
2023-11-10 07:27:25 +1100

    -updated verify password helper

--- helpers/verify_password_helper.go
+++ helpers/verify_password_helper.go
@@ -1,13 +1,9 @@
 package helpers
 
 import (
-	"fmt"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
 func VerifyPassword(encryptedPw string, password string) error {
-	fmt.Println("encryptedPw", encryptedPw)
-	fmt.Println("password", password)
 	return bcrypt.CompareHashAndPassword([]byte(encryptedPw), []byte(password))
 }
