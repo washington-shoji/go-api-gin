ea92b4706c486fc52a08a4e822fa3f29859bab2a
washington-shoji <shoji.washington@gmail.com>
2023-12-29 07:47:29 +1100

    -updated login handler added refresh token

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/washington-shoji/gin-api/config"
 	"github.com/washington-shoji/gin-api/helpers"
 	"github.com/washington-shoji/gin-api/models"
 	"github.com/washington-shoji/gin-api/services"
@@ -38,4 +41,32 @@ func (handler *LoginHandler) Login(ctx *gin.Context) {
 
 	ctx.Header("Content-Type", "application/json")
 	helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: result})
+}
+
+func (handler *LoginHandler) RefreshTokenEndpoint(ctx *gin.Context) {
+	refreshTokenString := helpers.ExtractToken(ctx)
+
+	token, err := jwt.Parse(refreshTokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("access denied")
+		}
+		return []byte(config.EnvConfig("JWT_SECRET")), nil
+	})
+	if err != nil {
+		helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusForbidden, Error: []string{"Not authorized"}})
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		username := fmt.Sprint(claims["username"])
+		newAccessToken, err := helpers.GenerateToken(username)
+		if err != nil {
+			ctx.Header("Content-Type", "application/json")
+			helpers.WebResponseSuccessHandler(ctx, helpers.ResponseSuccess{Status: http.StatusOK, Data: newAccessToken})
+		} else {
+			helpers.WebResponseError(ctx, helpers.ResponseError{Status: http.StatusForbidden, Error: []string{"Not authorized"}})
+			return
+		}
+	}
 }
